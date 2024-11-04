5092476ab39c25a4a241c8907e09ba8507c4f763
Igor Rocha <112200800+rochaoigor@users.noreply.github.com>
2024-11-04 19:28:37 -0300

    att

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import RedefinirSenha from './pages/RedefinirSenha';
 import Menu from './pages/Menu';
 import MeuPerfil from './pages/MeuPerfil';
 import Agendamento from './pages/Agendamento';
-import Reagendamento from './pages/Reagendamento';
 import Suporte from './pages/Suporte';
 import Logout from './pages/Logout';
+import Polo from './pages/Polo';
+import Administrador from './pages/Administrador';
+import './App.css';
 
 function App() {
   return (
+    <div className="containerBase">
+
+   
     <Router>
       <Routes>
       <Route path="/" element={<Navigate to="/Login" />} />
@@ -18,11 +23,13 @@ function App() {
         <Route path="/Menu" element={<Menu />} />
         <Route path="/MeuPerfil" element={<MeuPerfil />} />
         <Route path="/Agendamento" element={<Agendamento />} />
-        <Route path="/Reagendamento" element={<Reagendamento />} />
         <Route path="/Suporte" element={<Suporte />} />
         <Route path="/Logout" element={<Logout />} />
+        <Route path="/Polo" element={<Polo />} />
+        <Route path="/Administrador" element={<Administrador />} />
       </Routes>
     </Router>
+    </div>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Altere para a URL do back-end
+  baseURL: 'http://localhost:8080',
 });
 
 export const login = async (email, senha) => {
